1422f3c3090e3a9d474404619ef8e5fbe00007d2
menna-halawa <menna.halawa@hotmail.com>
2026-09-13 16:16:48 -0400

    Adjusted sub_warp_counter to hold whenever the writeback buffer is full

--- src/front_end/sub_warp_counter.sv
+++ src/front_end/sub_warp_counter.sv
@@ -2,31 +2,26 @@ module sub_warp_counter(
     input logic clk,
     input logic reset,
     input logic fetch_valid, // from fetch_stage — hold at 3 instead of resetting to 0 for invalid fetch
-    input logic decode_stall,
+    input logic buffer_full, // from writeback_arbiter - stall (hold current warp and make it not valid)
     output logic [1:0] sub_warp_cycle,
     output logic sub_warp_valid
 );
-    logic prev_at_3;
+    logic hold_d, hold_q;
+    assign hold_d = buffer_full || (sub_warp_cycle == 2'd3 && !fetch_valid);
     always_ff @(posedge clk) begin
         if (reset) begin
             sub_warp_cycle <= 2'd3;
-            prev_at_3 <= 1'b1;
+            hold_q <= 1'b1;
         end else begin
-            prev_at_3 <= (sub_warp_cycle == 2'd3);
-            // If the last cycle for the current instruction is reached,
-            // The counter resets to 0 if there is a valid instruction fetched
-            // Otherwise stay at 3 but the cycle becomes invalid to avoid
-            // repeat the current instruction
-
-            // If a decode stalled due to full FIFO in writeback_arbiter
-            // Warp is switched and the counter is reset to 0 or
-            // stays at 3 depending on if the next warp's instruction is fetched
-            if (decode_stall || sub_warp_cycle == 2'd3)
-                sub_warp_cycle <= fetch_valid ? 2'd0 : 2'd3;
+            hold_q <= hold_d;
+            if (hold_d)
+                sub_warp_cycle <= sub_warp_cycle;
+            else if (sub_warp_cycle == 2'd3)
+                sub_warp_cycle <= 2'd0;
             else
-                sub_warp_cycle <= sub_warp_cycle + 2'd1; // (sub_warp_cycle != 2'b11)
+                sub_warp_cycle <= sub_warp_cycle + 2'd1;
         end
     end
 
-    assign sub_warp_valid = ~(&sub_warp_cycle && prev_at_3); // (sub_warp_cycle == 2'b11 && previous cycle also no. 3)
+    assign sub_warp_valid = ~hold_q;
 endmodule
