efbbc125b44e4e875e22cf78c452d235a862cc96
menna-halawa <menna.halawa08@gmail.com>
2026-09-18 10:17:05 -0400

    Altered mask_split_unit based on the definition of the valid bit in the decode stage

--- src/control/mask_split_unit.sv
+++ src/control/mask_split_unit.sv
@@ -6,8 +6,8 @@ module mask_split_unit (
 
     // Information about the branch
     input logic is_branch,
+    input logic [7:0] is_valid,
     input logic [7:0] branch_taken_bits, // 1 bit per lane (8 total)
-    input logic [31:0] current_active_mask, // only currently active lanes can stay active
     // Info for calculating reconvergence PC
     input logic [31:0] branch_pc,
     input logic [5:0] push2_pc_recon_offset,
@@ -20,34 +20,41 @@ module mask_split_unit (
     output logic [31:0] push2_pc_fallthrough, // branch_pc + 4
     output logic [31:0] push2_recon_pc // branch_pc + push2_pc_recon_offset
 );
-    logic [31:0] taken_bits_accum;
     logic accum_done;
     assign accum_done = (&sub_warp_cycle && sub_warp_valid);
 
     always_ff @(posedge clk) begin
         if (reset) begin
-            taken_bits_accum <= 32'b0;
+            push2_mask_taken <= 32'b0;
+            push2_mask_not_taken <= 32'b0;
         end else if (is_branch) begin
             case (sub_warp_cycle)
-                2'd0: taken_bits_accum[7:0] <= branch_taken_bits;
-                2'd1: taken_bits_accum[15:8] <= branch_taken_bits;
-                2'd2: taken_bits_accum[23:16] <= branch_taken_bits;
-                2'd3: taken_bits_accum[31:24] <= sub_warp_valid ? branch_taken_bits : taken_bits_accum[31:24];
+                2'd0: begin
+                    push2_mask_taken[7:0] <= sub_warp_valid ? is_valid & branch_taken_bits : push2_mask_taken[7:0];
+                    push2_mask_not_taken[7:0] <= sub_warp_valid ? is_valid & ~branch_taken_bits : push2_mask_not_taken[7:0];
+                end
+                2'd1: begin
+                    push2_mask_taken[15:8] <= sub_warp_valid ? is_valid & branch_taken_bits : push2_mask_taken[15:8];
+                    push2_mask_taken[15:8] <= sub_warp_valid ? is_valid & ~branch_taken_bits : push2_mask_not_taken[15:8];
+                end
+                2'd2: begin
+                    push2_mask_taken[23:16] <= sub_warp_valid ? is_valid & branch_taken_bits : push2_mask_taken[23:16];
+                    push2_mask_taken[23:16] <= sub_warp_valid ? is_valid & ~branch_taken_bits : push2_mask_not_taken[23:16];
+                end
+                2'd3: begin
+                    push2_mask_taken[31:24] <= sub_warp_valid ? is_valid & branch_taken_bits : push2_mask_taken[31:24];
+                    push2_mask_taken[31:24] <= sub_warp_valid ? is_valid & ~branch_taken_bits : push2_mask_not_taken[31:24];
+                end
             endcase
         end
     end
 
     always_comb begin
         if (reset || ~accum_done) begin
-            push2_valid = 1'b0;
-            push2_mask_taken = 32'b0;
-            push2_mask_not_taken = 32'b0;
             push2_pc_taken = 32'b0;
             push2_pc_fallthrough = 32'b0;
             push2_recon_pc = 32'b0;
         end else if (accum_done) begin
-            push2_mask_taken = current_active_mask & taken_bits_accum;
-            push2_mask_not_taken = current_active_mask & ~taken_bits_accum;
             push2_pc_taken = pc_taken;
             push2_pc_fallthrough = branch_pc + 32'd4;
             push2_recon_pc = branch_pc + {{26{push2_pc_recon_offset[5]}}, push2_pc_recon_offset};
--- src/execution/int_alu.sv
+++ src/execution/int_alu.sv
@@ -2,6 +2,11 @@
 module int_alu (
     input logic clk,
     input logic valid_in,
+    input logic sub_warp_valid_in,
+    input logic [1:0] sub_warp_cycle_in,
+    input logic [9:0] reg_bank_addr_in,
+    input logic [31:0] branch_pc_in,
+
     input logic [5:0] opcode,
     input logic [31:0] src1,
     input logic [31:0] src2,
@@ -13,15 +18,14 @@ module int_alu (
     output logic branch_taken,
     output logic [31:0] result,
 
-    // Input warp_id, thread_slot, and dest_register for writeback_arbiter
-    input logic [9:0] reg_bank_addr_in,
-
     // Output warp_id, thread_slot, and dest_register for writeback_arbiter
     // This is simply the input passed to the output
-    output logic [9:0] reg_bank_addr_out
+    output logic sub_warp_valid_out,
+    output logic [1:0] sub_warp_cycle_out,
+    output logic [9:0] reg_bank_addr_out,
 );
     // Define parameters for instruction types
-    localparam R_TYPE = 3'b000, I_TYPE = 3'b001, FMA = 3'b010, BRANCH = 3'b011, SPECIAL = 3'b100, DONE = 3'b111;
+    localparam R_TYPE = 3'b000, I_TYPE = 3'b001, FMA = 3'b010, BRANCH = 3'b011, DONE = 3'b111;
 
     // Define parameters for specific instructions
     // R-type instructions
@@ -38,52 +42,68 @@ module int_alu (
     localparam [2:0] BEQ = 3'b000, BNE = 3'b001;
     localparam [2:0] BLT = 3'b010, BGE = 3'b011;
 
-    always_comb begin
+    always_ff begin
         if (reset) begin
-            valid_out = 1'b0;
-            branch_taken = 1'b0;
-            result = 32'b0;
+            valid_out <= 1'b0;
+            branch_taken <= 1'b0;
+            result <= 32'b0;
         end else begin
-            valid_out = valid_in;
-            branch_taken = 1'b0;
-
+            branch_taken <= 1'b0;
             case (opcode[5:3]) // case by opcode
                 R_TYPE : case (opcode[2:0]) // case by sub opcode
-                    NOP : result = 32'b0;
-                    ADD : result = src1 + src2;
-                    SUB : result = src1 - src2;
-                    MUL : result = src1 * src2;
-                    AND : result = src1 & src2;
-                    OR  : result = src1 | src2;
-                    SLT : result = $signed(src1) < $signed(src2);
-                    default: result = 32'b0;
+                    NOP : result <= 32'b0;
+                    ADD : result <= src1 + src2;
+                    SUB : result <= src1 - src2;
+                    MUL : result <= src1 * src2;
+                    AND : result <= src1 & src2;
+                    OR  : result <= src1 | src2;
+                    SLT : result <= $signed(src1) < $signed(src2);
+                    default: result <= 32'b0;
                 endcase
 
                 I_TYPE : case (opcode[2:0])
-                    LUI  : result = {src1[15:0], 16'b0};
+                    LUI  : result <= {src1[15:0], 16'b0};
                     LOAD, STORE,
-                    ADDI : result = src1 + src2;
-                    SUBI : result = src1 - src2;
-                    ANDI : result = src1 & src2;
-                    ORI  : result = src1 | src2;
-                    SLTI : result = $signed(src1) < $signed(src2);
-                    default : result = 32'b0;
+                    ADDI : result <= src1 + src2;
+                    SUBI : result <= src1 - src2;
+                    ANDI : result <= src1 & src2;
+                    ORI  : result <= src1 | src2;
+                    SLTI : result <= $signed(src1) < $signed(src2);
+                    default : result <= 32'b0;
                 endcase
 
                 BRANCH : begin
-                    result = PC + {{22{offset[15]}}, offset[15:6]};
+                    result <= PC + {{22{offset[15]}}, offset[15:6]};
                     case (opcode[2:0])
-                        BEQ : branch_taken = (src1 == src2);
-                        BNE : branch_taken = (src1 != src2);
-                        BLT : branch_taken = ($signed(src1) < $signed(src2));
-                        BGE : branch_taken = ($signed(src1) >= $signed(src2));
+                        BEQ : branch_taken <= (src1 == src2);
+                        BNE : branch_taken <= (src1 != src2);
+                        BLT : branch_taken <= ($signed(src1) < $signed(src2));
+                        BGE : branch_taken <= ($signed(src1) >= $signed(src2));
                     endcase
                 end
                 
-                default : result = 32'b0;
+                default : result <= 32'b0;
             endcase
         end
     end
 
-    assign reg_bank_addr_out = reg_bank_addr_in;
+    always_ff @(posedge clk) begin
+        if (reset) begin
+            sub_warp_cycle_out <= 2'b0;
+            sub_warp_valid_out <= 1'b0;
+            is_branch <= 1'b0;
+            valid_out <= 1'b0;
+            branch_pc_out <= 32'b0;
+            push2_pc_recon_offset <= 5'b0;
+            reg_bank_addr_out <= 10'b0;
+        end else begin
+            sub_warp_cycle_out <= sub_warp_cycle_in;
+            sub_warp_valid_out <= sub_warp_valid_in;
+            is_branch <= (opcode[5:3] == BRANCH);
+            valid_out <= valid_in;
+            branch_pc_out <= branch_pc_in;
+            push2_pc_recon_offset <= offset[5:0];
+            reg_bank_addr_out <= reg_bank_addr_in;
+        end
+    end
 endmodule
